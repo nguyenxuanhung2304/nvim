0a5a7a53199df04ea7b62a68ad490e0cf0a47feb
Hung <nguyenxuan.hung@rights-s.jp>
2022-09-23 20:19:40 +0700

    add packer plugin and improve startup time

---
+++ lua/plugins/init.lua
@@ -0,0 +1,10 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use "nvim-lua/plenary.nvim"
+
+  use "lewis6991/impatient.nvim"
+end)
