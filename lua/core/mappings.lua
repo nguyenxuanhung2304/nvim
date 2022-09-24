f6417b27ea6fd6a0058a0c360dcf1e1e51895857
Hung <nguyenxuan.hung@rights-s.jp>
2022-09-24 10:00:40 +0700

    add keymap for navigator between the windows

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,5 +1,30 @@
 local keymap = require("core.utils").keymap
 
+-- Telescope
 keymap("n", "<C-p>", "<cmd>Telescope find_files<cr>")
 keymap("n", "<C-f>", "<cmd>Telescope grep_string<cr>")
+
+-- NvimTree
 keymap("n", "<C-e>", "<cmd>NvimTreeToggle<cr>")
+
+-- Navigator between window
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Insert --
+-- Press jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
