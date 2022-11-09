ffe37905ac5d2859894fc90da1720b3ea8218c1e
Hung <nguyenxuan.hung@rights-s.jp>
2022-11-09 13:16:18 +0700

    fix: delete code unnecessary

--- lua/user/colorscheme/init.lua
+++ lua/user/colorscheme/init.lua
@@ -1,10 +1,1 @@
--- vim.cmd[[
---   let g:gruvbox_material_background = 'medium' " soft | medium | hard
---   let g:gruvbox_material_foreground = 'material' " mix | original | material
---   let g:gruvbox_material_transparent_background = 0
---
---   " For better performance
---   let g:gruvbox_material_better_performance = 1
--- ]]
-
 vim.cmd("colorscheme gruvbox-baby")
--- lua/user/plugins/configs/lualine/components.lua
+++ lua/user/plugins/configs/lualine/components.lua
@@ -1,5 +1,4 @@
 local M = {}
-local fn = vim.fn
 
 M.diagnostics = {
   "diagnostics",
