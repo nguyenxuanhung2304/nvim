526261588da48b4bac72eaf1478afb6356f7e6d8
Hung <nguyenxuan.hung@rights-s.jp>
2023-05-12 14:08:21 +0700

    change lualine

--- lua/user/plugins/configs/lualine/init.lua
+++ lua/user/plugins/configs/lualine/init.lua
@@ -2,27 +2,19 @@ local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then return end
 
 local components = require "user.plugins.configs.lualine.components"
-local transparent = require "user.plugins.configs.lualine.transparent"
-local lualine_theme
-
-if (vim.g.colors_name == 'gruvbox') then
-  lualine_theme = 'gruvbox'
-else
-  lualine_theme = transparent.theme()
-end
 
 lualine.setup({
 	options = {
 		icons_enabled = true,
-		theme = lualine_theme,
+		theme = "auto",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline", "toggleterm" },
 		always_divide_middle = true,
 	},
 	sections = {
-		lualine_a = { components.branch },
-		lualine_b = { components.diagnostics },
+		lualine_a = { "mode" },
+		lualine_b = { components.branch },
     lualine_c = { components.cwd },
 		lualine_x = { components.spaces, 'filetype', components.lsp },
 		lualine_y = { components.location },
