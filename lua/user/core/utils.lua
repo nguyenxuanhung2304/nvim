93ab63a35fbec59a76520d7cb5484f5d8457d70e
Hung <nguyenxuan.hung@rights-s.jp>
2023-05-14 09:30:22 +0700

    delete eval function in utils

--- lua/user/core/utils.lua
+++ lua/user/core/utils.lua
@@ -2,30 +2,12 @@ local M = {}
 
 function M.keymap(mode, lhs, rhs, opts)
   local options = { noremap = true, silent = true }
+
   if opts then
-      options = vim.tbl_extend("force", options, opts)
+    options = vim.tbl_extend("force", options, opts)
   end
+
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
-function M.eval()
-  local except_filetype = {"toggleterm", "fugitive"}
-  local filetype = vim.bo.filetype
-  for _, value in ipairs(except_filetype) do
-    if value == filetype then return '' end
-  end
-
-  local file_path = vim.api.nvim_eval_statusline('%f', {}).str
-  local modified = vim.api.nvim_eval_statusline('%M', {}).str == '+' and '⊚' or ''
-
-  file_path = file_path:gsub('/', ' > ')
-
-  return '%#WinBarPath#'
-   .. file_path
-   .. '%*'
-   .. '%#WinBarModified#'
-   .. modified
-   .. '%*'
 end
 
 return M
