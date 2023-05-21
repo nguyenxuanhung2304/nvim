3dca5ae529e79acd3b1588c0f01b270064e38c78
Nguyen Xuan Hung <nguyenxuan.hung@rights-s.jp>
2023-05-21 12:58:05 +0700

    enable transparent mode

--- lua/user/core/theme.lua
+++ lua/user/core/theme.lua
@@ -1,1 +1,6 @@
+local g = vim.g
+
+g.gruvbox_baby_telescope_theme = 1 -- Enable telescope theme
+g.gruvbox_baby_transparent_mode = 1 -- Enable transparent mode
+
 vim.cmd('colorscheme gruvbox-baby')
