8b6b2ab705d49afc8673d3b1ec3a6e74afbea668
Nguyen Xuan Hung <nguyenxuan.hung@rights-s.jp>
2023-06-06 18:27:40 +0700

    disable notification: Config Change Detected. Reloading...

--- init.lua
+++ init.lua
@@ -1,25 +1,26 @@
 vim.defer_fn(function()
-  pcall(require, "impatient")
+	pcall(require, "impatient")
 end, 0)
 
 -- Bootstraap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
- spec = {
-    { import = "user.plugins" }
-  }
+	spec = {
+		{ import = "user.plugins" },
+	},
+	change_detection = { enabled = false },
 })
 
 require("user.core")
