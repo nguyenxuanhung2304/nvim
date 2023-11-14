bbb7e22996ea60d700442ed05e2b4312e179c140
Nguyen Xuan Hung <mongtamquoc2015@gmail.com>
2023-11-14 16:31:50 +0900

    add: display cmdline and popup menu together

--- lua/user/plugins/editor.lua
+++ lua/user/plugins/editor.lua
@@ -321,6 +321,45 @@ return {
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false, -- add a border to hover docs and signature help
 			},
+			-- display the cmdline and popupmenu together: https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#display-the-cmdline-and-popupmenu-together
+			views = {
+				cmdline_popup = {
+					filter_options = {},
+					win_options = {
+						winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+					},
+					border = {
+						style = "none",
+						padding = { 2, 3 },
+					},
+					position = {
+						row = 5,
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = "auto",
+					},
+				},
+				popupmenu = {
+					relative = "editor",
+					position = {
+						row = 8,
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = 10,
+					},
+					border = {
+						style = "rounded",
+						padding = { 0, 1 },
+					},
+					win_options = {
+						winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+					},
+				},
+			},
 		},
 		dependencies = {
 			"MunifTanjim/nui.nvim",
