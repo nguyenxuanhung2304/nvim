3b43b97d98ec4ddb60307beec262217613c16449
Nguyen Xuan Hung <mongtamquoc2015@gmail.com>
2023-11-18 16:54:17 +0900

    add: use nvim-surround instead of mini-surround

--- lua/user/plugins/mini-surround.lua
+++
@@ -1,5 +0,0 @@
-return {
-	"echasnovski/mini.surround",
-	event = "BufReadPre",
-	opts = {},
-}
--- lua/user/plugins/nvim-recorder.lua
+++ lua/user/plugins/nvim-recorder.lua
@@ -2,4 +2,9 @@ return {
 	"chrisgrieser/nvim-recorder",
 	dependencies = "rcarriga/nvim-notify",
 	opts = {},
+	keys = {
+		-- these must match the keys in the mapping config below
+		{ "q", desc = " Start Recording" },
+		{ "Q", desc = " Play Recording" },
+	},
 }
