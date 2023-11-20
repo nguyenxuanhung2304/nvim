a47adf9ed8923e5ce3933feb44e02644a565eba4
Nguyen Xuan Hung <mongtamquoc2015@gmail.com>
2023-11-20 10:52:57 +0700

    fix: delete lazyloading for nvim-lastplace

--- lua/user/plugins/nvim-lastplace.lua
+++ lua/user/plugins/nvim-lastplace.lua
@@ -1,6 +1,5 @@
 return {
 	"ethanholz/nvim-lastplace",
-	event = "BufReadPre",
 	opts = {
 		lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
 		lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
