8ead767c011dea062ff49839d5bb47bb7317fdad
Nguyen Xuan Hung <mongtamquoc2015@gmail.com>
2023-11-21 11:56:47 +0700

    fix(auto-save): delete message when save file

--- lua/user/plugins/auto-save.lua
+++ lua/user/plugins/auto-save.lua
@@ -1,5 +1,11 @@
 return {
 	"Pocco81/auto-save.nvim",
 	event = "BufReadPre",
-	opts = {},
+	opts = {
+		execution_message = {
+			message = "",
+			dim = 0.18, -- dim the color of `message`
+			cleaning_interval = 200, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+		},
+	},
 }
