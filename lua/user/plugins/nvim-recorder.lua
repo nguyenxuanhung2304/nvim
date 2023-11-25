3e2d4d7d8ab808907785066431c154882b2a0dd5
Hung <mongtamquoc2015@gmail.com>
2023-11-25 09:42:21 +0700

    delete(plugin): nvim-recorder

--- lua/user/plugins/nvim-recorder.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"chrisgrieser/nvim-recorder",
-	dependencies = "rcarriga/nvim-notify",
-	opts = {},
-	keys = {
-		{ "q", desc = " Start Recording" },
-		{ "Q", desc = " Play Recording" },
-	},
-}
