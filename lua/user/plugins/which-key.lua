932cbf7c4a02bd651858ab2dab5f6899f174e8a4
Hung <mongtamquoc2015@gmail.com>
2023-12-14 16:11:20 +0900

    add(lazyload): which-key

--- lua/user/plugins/which-key.lua
+++ lua/user/plugins/which-key.lua
@@ -1,26 +1,27 @@
 return {
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-		end,
-		opts = {
-			defaults = {
-				mode = { "n", "v" },
-				["<leader>s"] = { name = "+Search" },
-				["<leader>z"] = { name = "+Fold" },
-				["<leader>C"] = { name = "+Conflict" },
-				["<leader>;"] = { name = "+Git" },
-				["<leader>D"] = { name = "+Diff" },
-			},
-			window = {
-				border = "single",
-			},
+	"folke/which-key.nvim",
+	opt = true,
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 100
+	end,
+	opts = {
+		defaults = {
+			mode = { "n", "v" },
+			["<leader>s"] = { name = "+Search" },
+			["<leader>z"] = { name = "+Fold" },
+			["<leader>C"] = { name = "+Conflict" },
+			["<leader>;"] = { name = "+Git" },
+			["<leader>D"] = { name = "+Diff" },
 		},
-		config = function(_, opts)
-			local wk = require("which-key")
-			wk.setup(opts)
-			wk.register(opts.defaults)
-		end,
-	}
+		window = {
+			border = "single",
+		},
+	},
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.setup(opts)
+		wk.register(opts.defaults)
+	end,
+	keys = "\\"
+}
