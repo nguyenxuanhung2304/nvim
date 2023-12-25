e4ac4f47a1d6f8e01b17748f0e8c6d228491a584
Hung <mongtamquoc2015@gmail.com>
2023-12-25 15:48:17 +0900

    add(plugin): text-case.nvim

---
+++ lua/user/plugins/text-case.lua
@@ -0,0 +1,11 @@
+return {
+	"johmsalas/text-case.nvim",
+	dependencies = { "nvim-telescope/telescope.nvim" },
+	config = function()
+		require("textcase").setup({})
+		require("telescope").load_extension("textcase")
+	end,
+	keys = {
+		{ "<leader>t", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "v" }, desc = "Text case" },
+	},
+}
