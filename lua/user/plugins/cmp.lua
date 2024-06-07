a4570a8c2b338ddb9668744b06b0f8178fdf2ff4
Hung <mongtamquoc2015@gmail.com>
2024-06-07 11:35:01 +0900

    add: remove lspkind

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -10,7 +10,6 @@ return {
 			"saadparwaiz1/cmp_luasnip",
 		},
 		opts = function()
-			local lspkind = require("lspkind")
 			local luasnip = require("luasnip")
 			local cmp = require("cmp")
 			require("luasnip/loaders/from_vscode").lazy_load()
