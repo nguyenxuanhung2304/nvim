3a09c68147378033264930f925b9c4e528cb3f5c
Hung <nguyenxuan.hung@rights-s.jp>
2024-08-20 19:55:18 +0700

    add(plugin): oil.nvim

--- lua/user/plugins/auto-save.lua
+++ lua/user/plugins/auto-save.lua
@@ -4,8 +4,14 @@ return {
 	opts = {
 		execution_message = {
 			message = "",
-			dim = 0.18, -- dim the color of `message`
-			cleaning_interval = 200, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+			dim = 0.18,
+			cleaning_interval = 200,
 		},
 	},
+	cond = function ()
+		local cur_buf_filetype = vim.bo.filetype
+		local is_oil = cur_buf_filetype == 'oil'
+
+		return is_oil
+	end
 }
---
+++ lua/user/plugins/oil.lua
@@ -0,0 +1,11 @@
+return {
+  'stevearc/oil.nvim',
+  opts = {
+    view_options = {
+      show_hidden = true
+    }
+  },
+  keys = {
+    {"-", "<CMD>Oil<CR>"}
+  }
+}
