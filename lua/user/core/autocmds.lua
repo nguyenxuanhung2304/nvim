66031fbef4db51bd8a54d25aaaefcee01a1deded
Hung <nguyenxuan.hung@rights-s.jp>
2024-12-19 13:50:27 +0700

    add: auto load file change by another text editor

--- lua/user/core/autocmds.lua
+++ lua/user/core/autocmds.lua
@@ -2,6 +2,24 @@ vim.cmd([[
   autocmd TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=300 }
 ]])
 
+vim.o.autoread = true
+vim.opt.updatetime = 1000
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+    pattern = "*",
+    callback = function()
+        if vim.fn.mode() ~= "c" then
+            vim.cmd('checktime')
+        end
+    end
+})
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+    pattern = "*",
+    callback = function()
+        vim.api.nvim_echo({
+            { "File changed on disk. Buffer reloaded.", "WarningMsg" }
+        }, true, {})
+    end
+})
 -- local luasnip_fix_augroup = vim.api.nvim_create_augroup("MyLuaSnipHistory", { clear = true })
 -- vim.api.nvim_create_autocmd("ModeChanged", {
 -- 	pattern = "*",
