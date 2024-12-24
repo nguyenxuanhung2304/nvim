6c945278c64cfd6901ab6396fa3000d56afec74d
Hung <mongtamquoc2015@gmail.com>
2024-12-24 10:23:33 +0700

    add: configure barbar.nvim to show only current buffer

--- lua/user/plugins/barbar.lua
+++ lua/user/plugins/barbar.lua
@@ -4,6 +4,13 @@ return {
 	init = function()
 		vim.g.barbar_auto_setup = false
 	end,
-	opts = {},
+	opts = {
+		animation = false,
+		hide = {
+			inactive = true,
+			visible = false,
+			current = false,
+		},
+	},
 	version = "^1.0.0", -- optional: only update when a new 1.x version is released
 }
