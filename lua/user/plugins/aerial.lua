ca00b60fa46bb49b40ceaccab11b76247389e003
Hung <mongtamquoc2015@gmail.com>
2024-12-28 14:43:01 +0700

    refactor(aerial): disable auto-open

--- lua/user/plugins/aerial.lua
+++ lua/user/plugins/aerial.lua
@@ -1,9 +1,7 @@
 return {
-  'stevearc/aerial.nvim',
-  opts = {
-    open_automatic = true,
-  },
-  keys = {
-    { "<leader>a", '<cmd>AerialToggle<CR>', desc = "Toggle Aerial" },
-  }
+	"stevearc/aerial.nvim",
+	opts = {},
+	keys = {
+		{ "<leader>a", "<cmd>AerialToggle<CR>", desc = "Toggle Aerial" },
+	},
 }
