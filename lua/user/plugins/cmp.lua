b5b5a5224dd0d5599ce3accbd15cd08dc6544997
Hung <mongtamquoc2015@gmail.com>
2025-01-06 10:45:24 +0700

    refactor(plugin): use blink.cmp instead of cmp

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -1,116 +1,105 @@
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-nvim-lua",
-			"saadparwaiz1/cmp_luasnip",
+	"saghen/blink.cmp",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		{
+			"xzbdmw/colorful-menu.nvim",
+			config = function()
+				-- You don't need to set these options.
+				require("colorful-menu").setup({
+					ls = {
+						lua_ls = {
+							-- Maybe you want to dim arguments a bit.
+							arguments_hl = "@comment",
+						},
+						gopls = {
+							-- When true, label for field and variable will format like "foo: Foo"
+							-- instead of go's original syntax "foo Foo".
+							add_colon_before_type = false,
+						},
+						["typescript-language-server"] = {
+							extra_info_hl = "@comment",
+						},
+						["typescript-tools"] = {
+							extra_info_hl = "@comment",
+						},
+						ts_ls = {
+							extra_info_hl = "@comment",
+						},
+						tsserver = {
+							extra_info_hl = "@comment",
+						},
+						vtsls = {
+							extra_info_hl = "@comment",
+						},
+						["rust-analyzer"] = {
+							-- Such as (as Iterator), (use std::io).
+							extra_info_hl = "@comment",
+						},
+						clangd = {
+							-- Such as "From <stdio.h>".
+							extra_info_hl = "@comment",
+						},
+						roslyn = {
+							extra_info_hl = "@comment",
+						},
+
+						-- If true, try to highlight "not supported" languages.
+						fallback = true,
+					},
+					-- If the built-in logic fails to find a suitable highlight group,
+					-- this highlight is applied to the label.
+					fallback_highlight = "@variable",
+					-- If provided, the plugin truncates the final displayed text to
+					-- this width (measured in display cells). Any highlights that extend
+					-- beyond the truncation point are ignored. Default 60.
+					max_width = 60,
+				})
+			end,
 		},
-		opts = function()
-			local luasnip = require("luasnip")
-			local cmp = require("cmp")
-			require("luasnip/loaders/from_vscode").lazy_load()
-			require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/lua/user/snippets" })
-
-			local col = vim.fn.col(".") - 1
-			local check_backspace = col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-
-			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = "buffer" },
-				},
-			})
-
-			return {
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body) -- For `luasnip` users.
-					end,
-				},
-				mapping = {
-					["<C-k>"] = cmp.mapping.select_prev_item(),
-					["<C-j>"] = cmp.mapping.select_next_item(),
-					["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-					["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-					["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-					["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-					["<C-e>"] = cmp.mapping({
-						i = cmp.mapping.abort(),
-						c = cmp.mapping.close(),
-					}),
-					["<CR>"] = cmp.mapping.confirm({ select = false }),
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.expandable() then
-							luasnip.expand()
-						elseif luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
-						elseif check_backspace then
-							fallback()
-						else
-							fallback()
-						end
-					end, {
-						"i",
-						"s",
-						c = cmp.config.disable,
-					}),
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, {
-						"i",
-						"s",
-					}),
-				},
-				formatting = {
-					fields = { "kind", "abbr", "menu" },
-					format = function(entry, vim_item)
-						local kind =
-							require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-						local strings = vim.split(kind.kind, "%s", { trimempty = true })
-						kind.kind = " " .. (strings[1] or "") .. " "
-						kind.menu = "    (" .. (strings[2] or "") .. ")"
-
-						return kind
-					end,
-				},
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "luasnip", priority = 8 },
-					{ name = "buffer" },
-					{ name = "path" },
-				},
-				confirm_opts = {
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = false,
-				},
-				window = {
-					documentation = {
-						border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-					},
-					completion = {
-						col_offset = -3,
-						side_padding = 0,
-						border = "rounded",
-						winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
-					},
-				},
-				experimental = {
-					ghost_text = false,
-					native_menu = false,
-				},
-			}
-		end,
 	},
+	version = "*",
+	opts = {
+		-- 'default' for mappings similar to built-in completion
+		-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+		-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+		-- See the full "keymap" documentation for information on defining your own keymap.
+		keymap = { preset = "default" },
+
+		appearance = {
+			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
+			-- Useful for when your theme doesn't support blink.cmp
+			-- Will be removed in a future release
+			use_nvim_cmp_as_default = true,
+			-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
+			nerd_font_variant = "mono",
+		},
+
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+	},
+	opts_extend = { "sources.default" },
+	config = function()
+		require("blink.cmp").setup({
+			completion = {
+				menu = {
+					draw = {
+						-- We don't need label_description now because label and label_description are already
+						-- conbined together in label by colorful-menu.nvim.
+						columns = { { "kind_icon" }, { "label", gap = 1 } },
+						components = {
+							label = {
+								text = require("colorful-menu").blink_components_text,
+								highlight = require("colorful-menu").blink_components_highlight,
+							},
+						},
+					},
+				},
+			},
+		})
+	end,
 }
