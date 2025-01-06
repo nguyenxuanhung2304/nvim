0e18ac79b4f9a005e684b7b7117033666923eede
Hung <mongtamquoc2015@gmail.com>
2025-01-06 11:11:26 +0700

    refactor(autocmds): add desc

--- lua/user/core/autocmds.lua
+++ lua/user/core/autocmds.lua
@@ -1,24 +1,44 @@
-vim.cmd([[
-  autocmd TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=300 }
-]])
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight text on yank",
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 300 })
+	end,
+})
 
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
-    pattern = "*",
-    callback = function()
-        if vim.fn.mode() ~= "c" then
-            vim.o.autoread = true
-            vim.opt.updatetime = 1000
-            vim.cmd('checktime')
-        end
-    end
+	desc = "Auto reload file when changed externally",
+	pattern = "*",
+	callback = function()
+		if vim.fn.mode() ~= "c" then
+			vim.o.autoread = true
+			vim.opt.updatetime = 1000
+			vim.cmd("checktime")
+		end
+	end,
 })
 
 vim.api.nvim_create_autocmd("FileChangedShellPost", {
-    pattern = "*",
-    callback = function()
-        vim.api.nvim_echo({
-            { "File changed on disk. Buffer reloaded.", "WarningMsg" }
-        }, true, {})
-    end
+	desc = "Show notification when file changed",
+	pattern = "*",
+	callback = function()
+		vim.api.nvim_echo({
+			{ "File changed on disk. Buffer reloaded.", "WarningMsg" },
+		}, true, {})
+	end,
 })
 
+vim.api.nvim_create_autocmd("WinNew", {
+	desc = "Set main window to 3/5 and split window to 2/5 of screen width",
+	pattern = "*",
+	callback = function()
+		if vim.o.splitright then
+			vim.cmd("wincmd =")
+
+			vim.cmd("wincmd h")
+			vim.cmd("vertical resize " .. math.floor(vim.o.columns * 3 / 5))
+
+			vim.cmd("wincmd l")
+		end
+	end,
+})
